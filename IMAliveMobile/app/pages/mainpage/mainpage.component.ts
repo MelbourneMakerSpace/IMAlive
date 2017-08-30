3a283177315d61876fe2804a3a029515186eba48
Tony Bellomo <tony.bellomo@gmail.com>
2017-08-29 21:27:01 -0400

    added ability to get lat/long when user clicks ok to show location

--- IMAliveMobile/app/pages/mainpage/mainpage.component.ts
+++ IMAliveMobile/app/pages/mainpage/mainpage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { getCurrentLocation, isEnabled, enableLocationRequest, Location } from "nativescript-geolocation"
+import { Accuracy } from "ui/enums"
+import { PropertyChangeData } from "data/observable";
+import * as switchModule from "tns-core-modules/ui/switch";
 
 @Component({
 	selector: 'mainpage',
@@ -14,6 +18,33 @@ export class MainpageComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() { }
+
+	getLocation() {
+		console.log("get location called...")
+		let location: Location;
+		getCurrentLocation({
+			desiredAccuracy: Accuracy.high, timeout: 20000
+		}).then(x => console.log("Lat:", x.latitude, " Lon:", x.longitude)).catch(err => console.log(err));
+
+	}
+
+	switchLatLong(event: PropertyChangeData) {
+		let myswitch: switchModule.Switch = <switchModule.Switch>event.object;
+		this.useLatLong = !myswitch.checked;
+		//console.log("latlong ok? ", this.useLatLong);
+		if (!isEnabled()) {
+			console.log("asking permission...")
+			enableLocationRequest().then(x => this.getLocation());
+		} else {
+			console.log("we have permission already");
+			this.getLocation();
+		}
+
+
+
+
+
+	}
 
 	chatNow(event) {
 		//console.dir(event);
