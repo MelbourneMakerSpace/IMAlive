620185c903ec1c2f4c691f227371b9c9f8e8bc8b
Tony Bellomo <tony.bellomo@gmail.com>
2018-02-06 19:48:49 -0500

    fixed chat list to include chat key with ngFirebase2

--- IMAliveWeb/src/app/components/chat-list/chat-list.component.ts
+++ IMAliveWeb/src/app/components/chat-list/chat-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Input, Output, Pipe, PipeTransform } from '@angular/core';
-import { AngularFireDatabase } from "angularfire2/database";
-import { Chat } from "models/chat";
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Chat } from 'models/chat';
 
 @Component({
   selector: 'im-chat-list',
@@ -15,15 +15,12 @@ export class ChatListComponent implements OnInit {
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-    this.db.list('activeChats').valueChanges().subscribe(result => {
+    this.db.list<Chat[]>('activeChats').snapshotChanges().subscribe(results => {
 
-      this.activeChats = result;
-      // console.dir(this.activeChats);
-      // try {
-      //   this.chatWindow.nativeElement.scrollTop = this.chatWindow.nativeElement.scrollHeight;
-      // } catch (err) { console.log("can't scroll?", err) }
-
-    });
+      this.activeChats = results.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    }
+    );
+    //console.dir(activechats);
   }
 
   chatNow(chatKey: string) {
